7d3d06ce4d20407db7c42bcdc42636480d65ad5b
Illussioner <illussionerstryder@gmail.com>
2026-05-24 22:48:58 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int res=-1;
+        for(int i=0; i<nums.size(); i++){
+            if(count ==0){
+                res = nums[i];
+            }
+            count += (res==nums[i])?1:-1;
+        }
+        return res;
+    }
+};
