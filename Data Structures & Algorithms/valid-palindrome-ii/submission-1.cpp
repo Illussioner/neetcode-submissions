07c0118a71df3c274acdc301ae6ae82c3fc08e18
Illussioner <illussionerstryder@gmail.com>
2026-05-31 21:23:14 +0530

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int l = 0, r=s.length()-1;
+        while(l < r){
+            if(s[l]!=s[r]){
+                return isPalindrome(s.substr(0,l) +
+                s.substr(l+1)) ||
+                isPalindrome(s.substr(0,r)+
+                s.substr(r+1));
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+private:
+    bool isPalindrome(const string& s){
+        int left = 0, right = s.length()-1;
+        while(left < right){
+            if(s[left]!= s[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
